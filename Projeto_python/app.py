8ccb7fb3f6450ba9d175e8be0d660abc7584ca33
Gabriel Neto <josegabriel9211@gmail.com>
2025-05-04 12:39:30 -0300

    Update app.py

--- Projeto_python/app.py
+++ Projeto_python/app.py
@@ -1,2 +1,18 @@
-print("funciono")
+# from datetime import datetime
 
+# postagem = datetime.strptime(input('Digite a data da postagem (dd/mm/aaaa): '), '%d/%m/%Y')
+# print(type(postagem))
+
+# data_inicio = datetime.now()
+# data_entrega = postagem
+
+# tempo = data_inicio - data_entrega
+# print(f'Tempo restante para a entrega: {tempo}')
+
+listagem = ['fone', 'celular', 'computador', 'nn', 'e']
+
+for listagem in listagem:
+    print(listagem)
+    if listagem == 'e':
+        break
+    
